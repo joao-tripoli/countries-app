72bdacf02bc8e06b8d86c24c3715bef617ac8055
João Matheus Tripoli <joao.tripoli.dev@gmail.com>
2025-07-28 15:13:54 -0300

    Remove api url from .env for the project to be easier to run

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,7 @@
+const API_URL = 'http://localhost:3001/api';
+
 export const fetcher = async (url: string) => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/${url}`);
+  const res = await fetch(`${API_URL}/${url}`);
 
   if (!res.ok) {
     throw new Error(`Failed to fetch weather data: ${res.statusText}`);
